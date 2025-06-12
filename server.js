e3a24b26bb48d7c37f16d584761a46695b97a160
Muhammad Furqan <muhammadfurqanch517@gmail.com>
2025-06-12 13:22:30 +0500

    Update server.js

--- server.js
+++ server.js
@@ -130,9 +130,9 @@ app.post("/send-data-dhanani", (req, res) => {
   const userMailOptions = {
     from: "muhammadfurqanch517@gmail.com",
     to: email,
-    subject: "Thank You for Contacting Muhammad Furqan",
+    subject: "Thank You for Contacting dhanai",
     html: `
-      <center><img src="https://res.cloudinary.com/dzcecmml3/image/upload/v1745834808/furqan-img_t5ptqh.jpg" alt="Muhammad Furqan Logo" style="width: 70px; height:70px; border-radius:50%; border:2px solid #f28301; padding:5px;"></center>
+      <center><img src="https://res.cloudinary.com/dzcecmml3/image/upload/v1749712071/logo_hqwxdf.png" alt="Muhammad Furqan Logo" style="width: 70px; height:70px; border-radius:50%; border:2px solid #f28301; padding:5px;"></center>
 
       <center><h2 style="font-family: Arial, sans-serif; font-size: 24px; font-weight: bold;">Hi ${name},</h2></center>
 
